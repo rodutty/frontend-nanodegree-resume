03613b41bf8d908f3df145c17e57ddb41ab3613d
Rob Dutka <rodutty@gmail.com>
2015-02-04 09:11:08 +0530

    Added template replacements for name and role

---
+++ js/resumeBuilder.js
@@ -0,0 +1,10 @@
+$("#main").append("Rob Dutka");
+var awesomeThoughts = "My name is Rob and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
+var formattedName = HTMLheaderName.replace("%data%", "Rob Dutka");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$(#header).append(formattedName);
+$(#header).append(formattedRole);
+
+
