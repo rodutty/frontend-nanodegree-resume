50860b368a0bdf8b4cd58c00f15740a940ddecef
Rob Dutka <rodutty@gmail.com>
2015-02-06 21:56:01 +0530

    Way too many commits! - Clean resumeBuilder.js - added profile image and images of connect project and CFLI process diagram

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,149 @@
-$("#main").append("Rob Dutka");
-var awesomeThoughts = "My name is Rob and I am AWESOME!";
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
-var formattedName = HTMLheaderName.replace("%data%", "Rob Dutka");
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-$(#header).append(formattedName);
-$(#header).append(formattedRole);
+var bio = {
+    "name": "Rob Dutka",
+    "role": "Web Devloper",
+    "welcomeMessage": "I wanna rock right now!",
+    "contacts": {
+        "mobile": "+91 011 88263 21188",
+        "email": "rodutty@gmail.com",
+        "github": "rodutty",
+        "location": "New Delhi"
+    },
+    "skills": [
+        "Brainstorming",
+        " Strategic Communications",
+        " Project Management",
+        " Financial Management",
+        " Web Design"
+    ]
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Department of Foreign Affairs, Trade and Development Canada (DFATD)",
+            "title": "Second Secretary (Public Affairs), High Commission of Canada to India",
+            "location": "New Delhi, India",
+            "dates": "July 2014 - Present",
+            "description": "Responsible for public affairs for Canada's High Commission to India, managed the Canada Fund for Local Initiatives (over $400,000), oversaw social media content, press releases etc."
+        },
+        {
+            "employer": "Department of Foreign Affairs, Trade and Development Canada (DFATD)",
+            "title": "Program Analyst, Stabilization and Reconstruction Task Force",
+            "location": "Ottawa, Canada",
+            "dates": "September 2010  - June 2014",
+            "description": "Managed the Police Reform and Justice envelopes ($7-12.5 million of funding annually) of the Global Peace and Security Fund's Asia Team"
+        },
+        {
+            "employer": "Department of Foreign Affairs, Trade and Development Canada (DFATD)",
+            "title": "Field Operations Officer, Afghanistan Task Force",
+            "location": "Ottawa, Canada",
+            "dates": "April 2009 - September 2010",
+            "description": "Provide human resources support for government personnel deploying to Afghanistan. It's both more complicated and boring than it sounds."
+        }
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            "title": "Connect Online",
+            "dates": "2014",
+            "description": "When I arrived at the High Commission of Canada in India, one of our public affairs products was a monthly newsletter 'Connect' which featured highlights from the previous month on key aspects of Canada and India's relationship). I realized that the conversation with our audience was one-way and that we couldn't get great metrics from the email newsletter.",
+            "images": [
+                "images/connect.tiff"
+            ]
+        },
+        {
+            "title": "Reimagining CLFI Management",
+            "dates": "Spring 2015",
+            "description": "Our application process was screwy, and needed to be fixed! We had a huge pot of money to spend, and were waiting for endless approvals before even beginning to plan how to spend it. The revised process envisioned an ongoing application process that would feed a hopper of projects. These projects would be validated by our selection committee well in advance of us receiving funds from HQ. In this manner, we would be in a position to respond quickly once we receive funds.",
+            "images": [
+                "images/cfli.jpg"
+            ]
+        }
+    ]
+}
+
+var education = {
+    "schools": [
+        {
+            "name": "Royal Military College",
+            "location": "Kingston",
+            "degree": "",
+            "majors": [
+                "Engineering"
+            ],
+            "dates": "September 2000 - February 2001",
+            "url": "http://www.rmc.ca/"
+        },
+        {
+            "name": "University of Windsor",
+            "location": "Windsor",
+            "degree": "",
+            "majors": [
+                "Human Kinetics"
+            ],
+            "dates": "September 2001 - April 2002",
+            "url": "http://www.uwindsor.ca/"
+        },
+        {
+            "name": "University of Waterloo",
+            "location": "Waterloo",
+            "degree": "",
+            "majors": [
+                "Mathematics"
+            ],
+            "dates": "September 2002 - April 2003",
+            "url": "https://uwaterloo.ca/"
+        },
+        {
+            "name": "McMaster University",
+            "location": "Hamilton",
+            "degree": "Bachelor of Arts",
+            "majors": [
+                "Political Science"
+            ],
+            "dates": "September 2003 - April 2006",
+            "url": "http://www.mcmaster.ca/"
+        },
+        {
+            "name": "Royal Roads University",
+            "location": "Victoria",
+            "degree": "Graduate Certificate in Project Management",
+            "majors": [
+                "Project Management"
+            ],
+            "dates": "January 2010 - June 2010",
+            "url": "http://www.royalroads.ca/"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Intro to Computer Science",
+            "school": "Udacity",
+            "dates": "December 2014 - February 2015",
+            "url": "https://www.udacity.com/course/cs101"
+        },
+        {
+            "title": "Front-End Web Developer Nano-Degree",
+            "school": "Udacity",
+            "dates": "January 2015 - ???",
+            "url": "https://www.udacity.com/course/nd001"
+        }
+    ]
+}
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
 
 
